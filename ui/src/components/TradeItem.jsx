d257c8eeaed9236f7ee3d253587d7c8ef3db100e
Justin-Chua <justin.chua@ucalgary.ca>
2024-12-05 23:56:46 -0700

    added basic version of add trade item component

--- ui/src/components/TradeItem.jsx
+++ ui/src/components/TradeItem.jsx
@@ -2,12 +2,12 @@ import { Flex, Button, Text, Image } from "@chakra-ui/react";
 
 const TradeItem = () => {
     return (
-        <Button p="2rem" mt="1rem" h="80px" w="350px" bg="white">
+        <Button p="2rem" h="80px" w="350px" bg="white">
             <Image 
             src="https://seeklogo.com/images/A/atlanta-hawks-logo-A108D0AC8D-seeklogo.com.png"
             boxSize="50px" />
-            <Flex grow="1" direction="column" align="center" justify="center">
-                <Text>Zacharie Rissacher</Text>
+            <Flex grow="1" direction="column" gap="4px" align="center" justify="center">
+                <Text>Zaccharie Risacher</Text>
                 <Text fontWeight="light">$4,000,000</Text>
             </Flex>
         </Button>
--- ui/src/components/TradeSection.jsx
+++ ui/src/components/TradeSection.jsx
@@ -1,17 +1,18 @@
 import { Container, VStack } from "@chakra-ui/react";
 import TradeHeader from "./TradeHeader.jsx";
 import TradeItem from "./TradeItem.jsx";
+import AddTradeItem from "./AddTradeItem.jsx";
 
 const TradeSection = () => {
     return (
         <Container p="0" mx="2rem" minH="850px" minW="850px" bg="gray.300" borderRadius="2rem" overflow="hidden">
             <TradeHeader />
-            <VStack spacing="1rem">
+            <VStack spacing="1.5rem" mt="2rem">
                 <TradeItem />
                 <TradeItem />
                 <TradeItem />
                 <TradeItem />
-                <TradeItem />
+                <AddTradeItem />
             </VStack>
         </Container>
     );

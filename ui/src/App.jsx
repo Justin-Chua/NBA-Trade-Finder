35d30d4c688430b0c264c1d5daace73592929ece
Justin-Chua <justin.chua@ucalgary.ca>
2024-12-10 14:21:27 -0700

    adjusted style convention to match StandardJS

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,15 +1,14 @@
-import TradeSection from "./components/TradeSection.jsx";
-import { Flex } from "@chakra-ui/react";
+import TradeSection from './components/TradeSection.jsx'
+import { Flex } from '@chakra-ui/react'
 
 const App = () => {
   return (
-    <>
-      <Flex pt="2rem" direction="row" align="center" justify="center">
-        <TradeSection></TradeSection>
-        <TradeSection></TradeSection>
-      </Flex>
-    </>
+    <Flex pt='2rem' direction='row' align='center' justify='center'>
+      <TradeSection></TradeSection>
+      <TradeSection></TradeSection>
+    </Flex>
   )
-};
+}
 
-export default App;
+export default App
+
